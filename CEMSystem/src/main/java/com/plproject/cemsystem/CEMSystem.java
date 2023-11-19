8b200ffa33fe7aad4902573f1d5f1082cbdc538f
Tr0ph1c <ahmmahmouda767@gmail.com>
2023-11-19 12:26:49 +0200

    Initialization of the project
    added empty window and hello world

---
+++ CEMSystem/src/main/java/com/plproject/cemsystem/CEMSystem.java
@@ -0,0 +1,10 @@
+package com.plproject.cemsystem;
+
+public class CEMSystem {
+
+    public static void main(String[] args) {
+        LoginWindow login = new LoginWindow();
+        login.setVisible(true);
+        System.out.println("MAIN EXECUTED!");
+    }
+}
