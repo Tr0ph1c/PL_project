81914ac7f0dedffae1fadd21187762897b4a7474
Tr0ph1c <ahmmahmouda767@gmail.com>
2023-11-26 19:56:26 +0200

    simplified function

--- CEMSystemJava/src/DirectoryBasedDB/Database.java
+++ CEMSystemJava/src/DirectoryBasedDB/Database.java
@@ -31,24 +31,10 @@ public class Database {
             Logger.getLogger(Database.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
+    
     public static boolean writeRecord (String path, String RecordID, String[] lines) {
-        if (recordExists(path , RecordID)) return false;
-
-        File file = new File(path + RecordID);
-
-        try {
-            BufferedWriter writer = new BufferedWriter(new FileWriter(file));
-
-            for (int i = 0; i < lines.length; i++) {
-                writer.append(lines[i] + "\n");
-            }
-
-            writer.close();
-            return true;
-        } catch (IOException ex) {
-            Logger.getLogger(Database.class.getName()).log(Level.SEVERE, null, ex);
-            return false;
-        }
+          if(recordExists(path,RecordID)) {overwriteRecord(path,RecordID,lines);return true;}
+          else {return false;}
     }
 
     public static boolean removeRecord (String path, String RecordID) {
