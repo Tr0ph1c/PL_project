3d89375cf46d84151bec0861e2da255371378200
Ahmed164530 <kinggamal164@gmail.com>
2023-11-27 13:51:15 +0200

    complete the Login function in UserManagement.java

--- CEMSystemJava/src/cemsystemjava/UserManagement.java
+++ CEMSystemJava/src/cemsystemjava/UserManagement.java
@@ -1,5 +1,9 @@
 package cemsystemjava;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 enum UserType {
     STUDENT,
     LECTURER,
@@ -9,10 +13,36 @@ enum UserType {
 public class UserManagement {
     public static User currentUser;
     
-    public static boolean Login (int id, String password) {
-        //Check if ID and password match from database return false if not
-        //set User to an object with the login info
-        System.out.println("logging in");
-        return true;
+    public static int Login (int id, String password, UserType type)
+    {
+        String fileName = id + ".txt"; // add this line to get the file name based on the ID
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(fileName));
+            String line;
+            boolean found = false;
+            
+            while ((line = reader.readLine()) != null) {
+                String[] parts = line.split(",");
+                if (parts[0].equals(type.toString()) && Integer.parseInt(parts[1]) == id) {
+                    found = true;
+                    if (parts[2].equals(password)) {
+                        System.out.println("logging in");
+                        return 0;
+                    } else {
+                        System.out.println("wrong password");
+                        return 1;
+                    }
+                }
+            }
+            if (!found) {
+                System.out.println("user not found");
+                return 2;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return -1;
     }
 }
+
+
