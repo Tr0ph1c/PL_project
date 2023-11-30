452de625917f181f0c4b071d1affdd9aec30fc98
a7md01 <ahmed12.alnaggar34@gmail.com>
2023-11-30 16:00:31 +0200

    mianFunction

--- CEMSystemJava/src/cemsystemjava/CEMApp.java
+++ CEMSystemJava/src/cemsystemjava/CEMApp.java
@@ -2,10 +2,39 @@ package cemsystemjava;
 
 import DirectoryBasedDB.Database;
 import java.io.File;
+import java.util.Scanner;
 
 public class CEMApp {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int choice;
+
+        while (true) {
+            System.out.println("Main Menu:");
+            System.out.println("0 - Exit");
+            System.out.println("1 - Login");
+            System.out.print("Enter your choice: ");
+            choice = scanner.nextInt();
+
+            switch (choice) {
+                case 0:
+                    System.out.println("Exiting the program...");
+                    System.exit(0);
+                    break;
+                case 1:
+                    System.out.println("Login Menu:");
+                    System.out.println("1 - Admin");
+                    System.out.println("2 - Student");
+                    System.out.println("3 - Lecturer");
+                    System.out.print("Enter your choice: ");
+                    int loginChoice = scanner.nextInt();                    break;
+                default:
+                    System.out.println("Invalid choice. Please try again.");
+                    break;
+            }
         
+       }
     }
 }
+
