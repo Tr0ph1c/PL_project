c342842d9ec8ac92600dfa11642f7a8bbb52fb6a
xzbrhom7 <katafafa4@gmail.com>
2023-12-02 13:33:55 +0200

    تحديث كلاس الاسئلة

--- CEMSystemJava/src/cemsystemjava/Question.java
+++ CEMSystemJava/src/cemsystemjava/Question.java
@@ -1,13 +1,41 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package cemsystemjava;
 
 /**
  *
- * @author HUAWEI
+ * @ x bn z
  */
+import DirectoryBasedDB.Database;
+
 public class Question {
-    
+    private final String question;
+    private final String[] answers;
+    private final int correctAnswer;
+    private boolean isCorrect;
+
+    public Question(String question, String[] answers, int correctAnswer) {
+        this.question = question;
+        this.answers = answers;
+        this.correctAnswer = correctAnswer;
+        this.isCorrect = false;
+    }
+
+    public void answer(int ans) {
+        if (ans >= 1 && ans <= 4) {
+            if (ans - 1 == correctAnswer) {
+                isCorrect = true;
+            }
+        }
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder result = new StringBuilder();
+        result.append(question).append(";");
+        for (String answer : answers) {
+            result.append(answer).append(";");
+        }
+        result.append(correctAnswer).append(";").append(isCorrect);
+        return result.toString();
+    }
 }
