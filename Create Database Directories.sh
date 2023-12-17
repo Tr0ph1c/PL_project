8adf42e495cc2bd5c3d0bd97f6b0b6afe7b798f1
Tr0ph1c <ahmmahmouda767@gmail.com>
2023-12-17 13:38:23 +0200

    added shell script for linux users

---
+++ Create Database Directories.sh
@@ -0,0 +1,22 @@
+mkdir database
+cd database
+mkdir administrators
+pushd administrators
+echo "9000" > ID
+popd
+mkdir courses
+pushd courses
+cat > courses
+popd
+mkdir exams
+pushd exams
+echo "5000" > ID
+popd
+mkdir lecturers
+pushd lecturers
+echo "2000" > ID
+popd
+mkdir students
+pushd students
+echo "1000" > ID
+popd
--- createDBdir.bat
+++
@@ -1,7 +0,0 @@
-mkdir database
-cd database
-mkdir administrators
-mkdir courses
-mkdir exams
-mkdir lecturers
-mkdir students
